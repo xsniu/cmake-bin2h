913f5440672e9d2b9467eae71601e0538ec7c6a3
cool <niu199212cool@163.com>
2023-04-02 15:41:14 +0800

    add headr define

--- bin2h.cmake
+++ bin2h.cmake
@@ -70,9 +70,13 @@ function(BIN2H)
     string(MAKE_C_IDENTIFIER "${BIN2H_VARIABLE_NAME}" BIN2H_VARIABLE_NAME)
     string(TOUPPER "${BIN2H_VARIABLE_NAME}" BIN2H_VARIABLE_NAME)
 
-    # declares byte array and the length variables
+    set(header_define_start "#ifndef __${BIN2H_VARIABLE_NAME}_H__\n#define __${BIN2H_VARIABLE_NAME}_H__\n")
+    set(header_define_end "#endif \//__${BIN2H_VARIABLE_NAME}_H__\n")
     set(arrayDefinition "const unsigned char ${BIN2H_VARIABLE_NAME}[] = { ${arrayValues} };")
     set(arraySizeDefinition "const size_t ${BIN2H_VARIABLE_NAME}_SIZE = ${arraySize};")
+
+    set(declarations "${header_define_start}${arrayDefinition}\n\n${arraySizeDefinition}\n\n${header_define_end}")
+    
 
     set(declarations "${arrayDefinition}\n\n${arraySizeDefinition}\n\n")
     if(BIN2H_APPEND)
